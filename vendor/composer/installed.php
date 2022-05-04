4b2becb41ae3d1bef797a996adeefd780a4270f6
Juan <juan.308904@gmail.com>
2022-05-04 13:39:28 +0200

    Instalar composer en el poryecto. Conexion a bd.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '50c70e906ed4685f80b0b5601f4ba0bdda6497c7',
+        'name' => 'jaro/tienda-online',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jaro/tienda-online' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '50c70e906ed4685f80b0b5601f4ba0bdda6497c7',
+            'dev_requirement' => false,
+        ),
+    ),
+);
